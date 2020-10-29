5b9d9ce5ec2f4724e99b823dd28fde0acfaeec24
YoussraHazzout <YoussraHazzout@gmail.com>
2020-10-29 11:59:00 +0100

    Had eerst foutje gemaakt met bestand, stond niet op de juiste plek. Volgens mij is het nu wel goed.. Was even heel erg in de war :-)

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,24 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
-}
+    public static void main(String[] args) {
+        try {
+            File f = new File("Hello.txt");
+            Scanner reader = new Scanner(f);
+            while (reader.hasNextLine()){
+                System.out.println(reader.nextLine());
+            }
+            reader.close();
+
+        } catch(FileNotFoundException f){
+            System.out.println("Het bestand kan niet gevonden worden.");
+            f.printStackTrace();
+        }
+        }
+            //System.out.println("Het bestand is ingelezen!");
+            //static void bestandInlezen(String f) {
+
+            }
+
--- src/FilesInlezen2.java
+++ src/FilesInlezen2.java
@@ -1,4 +1,26 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen2 {
+
+    public static void main(String[] args) {
+        try {
+            BestandInlezen("Hello.txt");
+
+        } catch (FileNotFoundException f) {
+            System.out.println("Het bestand kan niet worden gelezen");
+            System.out.println("Foutmelding!:" + f.getMessage());
+        }
+    }
+
+    static void BestandInlezen(String file) throws FileNotFoundException {
+        File f = new File("Hello.txt");
+        Scanner reader = new Scanner(f);
+        while (reader.hasNextLine()) {
+            System.out.println(reader.nextLine());
+        }
+
+    }
 
 }
